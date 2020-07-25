ab943a922119a76205c78503c437d197f6023f09
Fazil <fazilmohammad258@gmail.com>
2020-07-26 01:30:57 +0530

    Modifief HomePage

--- src/Containers/Homepage/Home.js
+++ src/Containers/Homepage/Home.js
@@ -9,8 +9,8 @@ class Home extends Component {
             <div className={classes.Home}>
                   <Frontimage />  
                   <div className={classes.body}>
-                      <p style={{fontWeight:'570',fontSize:'290%'}}>What is AAS?</p>
-                      <p style={{marginTop:'10%',fontSize:'150%'}}>Accident Alert System is a device which collect data from the incident
+                      <p style={{fontWeight:'570',fontSize:'290%',textDecoration:'underline',color:'black'}}>What is AAS?</p>
+                      <p style={{marginTop: '0px',fontSize:'150%'}}>Accident Alert System is a device which collect data from the incident
                           via the help of sensors. The sensor analyze the impact and decides how much severe
                           the incident is. if the outcome of the data declare the incident to be severe then the system initializes Alert
                           and calling procedures.
